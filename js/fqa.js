52ab3f41db230b1fd648b0d1a1ccd9ec9565e02d
vandana8928 <vandanayadav8286@gmail.com>
2023-12-31 17:19:38 +0530

    vccvcvvcvc

--- js/fqa.js
+++ js/fqa.js
@@ -1,10 +1,14 @@
-answers = document.querySelectorAll(".accordion"); 
+const answers = document.querySelectorAll(".accordion"); 
 answers.forEach((event) => { 
+  
   event.addEventListener("click", () => { 
+
     if (event.classList.contains("active")) { 
     event.classList.remove("active"); 
+  
     } else { 
      event.classList.add("active"); 
+    
     } 
   }); 
   });
